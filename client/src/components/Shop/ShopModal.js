9e105c22b8b6c2df7cb7be95ddfefe49c5c080de
Tristan-Tribes <43269571+Tristan-T@users.noreply.github.com>
2022-03-05 11:53:47 +0100

    clicking outside shop modal now closes it

--- client/src/components/Shop/ShopModal.js
+++ client/src/components/Shop/ShopModal.js
@@ -3,20 +3,26 @@ import {useState} from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group"
 import ShopTile from "./ShopTile";
 import "./shopStyle.css"
+import OutsideClickHandler from "react-outside-click-handler";
 
-const ShopModal = ({showShop, kanjiListShop, unlockKanjis, canBuy}) => {
+const ShopModal = ({showShop, setShowShop, kanjiListShop, unlockKanjis, canBuy}) => {
     return <>{showShop ? (
-        <TransitionGroup
-            component="div"
-            id="shop-modal"
-            className="rounded-md overflow-y-scroll absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border-gray-400 border-thin rounded p-2 bg-gray-200 grid grid-cols-5 auto-rows-min gap-2 place-items-center w-4/5 h-3/4"
-            style={{pointerEvents: canBuy ? 'auto' : 'none'}}
-            >{kanjiListShop.map((v) => v.uses!==0?(
-                    <CSSTransition key={v.kanji} timeout={200} classNames="item">
-                        <ShopTile kanji={v} unlockKanjis={unlockKanjis}/>
-                    </CSSTransition>
-                ):null)}
-        </TransitionGroup>
+        <OutsideClickHandler onOutsideClick={() => {
+                setShowShop(false);
+            }
+        }>
+            <TransitionGroup
+                component="div"
+                id="shop-modal"
+                className="rounded-md overflow-y-scroll absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border-gray-400 border-thin rounded p-2 bg-gray-200 grid grid-cols-5 auto-rows-min gap-2 place-items-center w-4/5 h-3/4"
+                style={{pointerEvents: canBuy ? 'auto' : 'none'}}
+                >{kanjiListShop.map((v) => v.uses!==0?(
+                        <CSSTransition key={v.kanji} timeout={200} classNames="item">
+                            <ShopTile kanji={v} unlockKanjis={unlockKanjis}/>
+                        </CSSTransition>
+                    ):null)}
+            </TransitionGroup>
+        </OutsideClickHandler>
 
     ) : null}</>
 };
