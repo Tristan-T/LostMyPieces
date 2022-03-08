ade4f265aa80d4d801c448c05f7233546b5e123e
Tristan-Tribes <43269571+Tristan-T@users.noreply.github.com>
2022-03-08 15:22:33 +0100

    updated to https

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,25 +1,25 @@
 export function getKanjisList() {
-    return fetch("http://lostmypieces.com:3000/kanjis")
+    return fetch("https://lostmypieces.com:3000/kanjis")
 }
 
 export function getWordsList() {
-    return fetch("http://lostmypieces.com:3000/words")
+    return fetch("https://lostmypieces.com:3000/words")
 }
 
 export function getKanji(kanji) {
-    return fetch("http://lostmypieces.com:3000/kanjis/" + kanji)
+    return fetch("https://lostmypieces.com:3000/kanjis/" + kanji)
 }
 
 export function getWord(word) {
-    return fetch("http://lostmypieces.com:3000/words/" + word)
+    return fetch("https://lostmypieces.com:3000/words/" + word)
 }
 
 export function getNumberCombination() {
-    return fetch("http://lostmypieces.com:3000/getNumberCombination")
+    return fetch("https://lostmypieces.com:3000/getNumberCombination")
 }
 
 export function getMerge(input) {
-    return fetch("http://lostmypieces.com:3000/getMerge", {
+    return fetch("https://lostmypieces.com:3000/getMerge", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -29,7 +29,7 @@ export function getMerge(input) {
 }
 
 export function getShopCombination(kanjis) {
-    return fetch("http://lostmypieces.com:3000/getShopCombination", {
+    return fetch("https://lostmypieces.com:3000/getShopCombination", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -39,7 +39,7 @@ export function getShopCombination(kanjis) {
 }
 
 export function getKanjisUnlocked(kanjis) {
-    return fetch("http://lostmypieces.com:3000/getKanjisUnlocked", {
+    return fetch("https://lostmypieces.com:3000/getKanjisUnlocked", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
